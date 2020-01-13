3309ea11c8e240108d530db084134bd347c6ed4a
Noelle Everage <neverage84@yahoo.com>
2020-01-13 14:27:40 -0800

    Changed center alignment for section headings. Moved JS items to separate scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $('.slider').slider();
+  });
+
+  $(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+
+  $(document).ready(function(){
+    $('.pushpin').pushpin();
+  });
+  $(document).ready(function(){
+    $('.collapsible').collapsible();
+  });
+  $(document).ready(function(){
+    $('.tooltipped').tooltip();
+  });
+  $(document).ready(function(){
+    $('.scrollspy').scrollSpy();
+  });
+  $(document).ready(function(){
+    $('.parallax').parallax();
+  });
